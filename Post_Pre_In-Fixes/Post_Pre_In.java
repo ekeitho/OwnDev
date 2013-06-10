54b348a0d844f77cb9192480d69cc273c3a91499
Keith Abdulla <email@ekeitho.com>
2013-06-09 20:06:10 -0700

    Added Scanners and Delimiters and few output strings
    Learned that I can do useDelimiter("[+-/*]") to signify different
    delimiters. Such as if I had A+B-D, it'll use either + or - as the
    delimiter and print out ABD still.

--- Post_Pre_In-Fixes/Post_Pre_In.java
+++ Post_Pre_In-Fixes/Post_Pre_In.java
@@ -5,12 +5,45 @@ public class Post_Pre_In{
 
     public static void main(String[] args){
     
+        Stack<String> stack = new Stack<String>();
+    
     
         System.out.println("Please enter an algebraic equation that you would like to generate a fixation out of.");
+        System.out.println("Please leave spaces inbetween every symbol and character.");
+        System.out.println("Such as: ( A + B ) - D * ( C + D )");
+        
         Scanner scan = new Scanner(System.in);
-    
-    
-    
+        String equation = scan.nextLine();        
+        Scanner equation_scan = new Scanner(equation).useDelimiter(" ");
+        
+        while( equation_scan.hasNext() ){
+        
+            System.out.println( equation_scan.next() );
+        }
+        
+        
+        
+            
+        System.out.println("Please enter what kind of notation you'd like to generator your equation to.");
+        System.out.print("Postfix, Prefix, or Infix: ");
+        
+        Scanner scan2 = new Scanner(System.in);
+        String notation = scan.nextLine();
+        
+        if( notation.equals("Postfix") || notation.equals("postfix") ){
+        
+            System.out.println("This is postfix");
+        }
+        
+        else if( notation.equals("Prefix") || notation.equals("prefix") ){
+        
+            System.out.println("This is prefix");
+        }
+        
+        else if( notation.equals("Infix") || notation.equals("infix") ){
+        
+            System.out.println("This is infix");
+        }
     
     
     
