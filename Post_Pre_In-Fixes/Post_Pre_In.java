25afcbb7b37c5a74d8f1364237ec9892cb5284a3
Keith Abdulla <email@ekeitho.com>
2013-06-09 18:58:55 -0700

    Start of Post_Pre_In Generator Class
    The intention of this program is to turn an algebraic equation into a
    certain fixation in computer science.

---
+++ Post_Pre_In-Fixes/Post_Pre_In.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class Post_Pre_In{
+
+
+    public static void main(String[] args){
+    
+    
+        System.out.println("Please enter an algebraic equation that you would like to generate a fixation out of.");
+        Scanner scan = new Scanner(System.in);
+    
+    
+    
+    
+    
+    
+    
+    }
+
+
+
+
+
+}
