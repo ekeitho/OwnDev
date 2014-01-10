1c165ce8a95e49d097ab1bf6a6328a116bd427ec
Keith Abdulla <email@ekeitho.com>
2014-01-09 22:40:39 -0800

    took the route in writing try/catch for KeyError rather than using the dict.get() method that would return None. wanted to experience with exceptions

--- projects/URLget/testURL.py
+++ projects/URLget/testURL.py
@@ -5,3 +5,10 @@ if ekeitho.getValue("toast") == "pb":
 	print "Got Correct Value for ekeitho"
 else:
 	print "Wrong Value"
+
+try:
+	ekeitho.getValue("pb")
+except KeyError:
+	print "URL class passed the test with an unknown field"
+else:
+	print "Failed test and returned an unknown field somehow."
