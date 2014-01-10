c27d15c741473593843d46b4235b94891f35675a
Keith Abdulla <email@ekeitho.com>
2014-01-09 22:42:41 -0800

    made clarification to what passed and failed

--- projects/URLget/testURL.py
+++ projects/URLget/testURL.py
@@ -2,13 +2,13 @@ import URL
 	
 ekeitho = URL.Url("http://www.ekeitho.com?toast=pb")
 if ekeitho.getValue("toast") == "pb":
-	print "Got Correct Value for ekeitho"
+	print "Passed Test 1: Got Correct Value for ekeitho"
 else:
-	print "Wrong Value"
+	print "Failed Test 1: Wrong Value"
 
 try:
 	ekeitho.getValue("pb")
 except KeyError:
-	print "URL class passed the test with an unknown field"
+	print "Passed Test 2: URL class passed the test with an unknown field"
 else:
-	print "Failed test and returned an unknown field somehow."
+	print "Failed Test 2: returned an unknown field somehow."
